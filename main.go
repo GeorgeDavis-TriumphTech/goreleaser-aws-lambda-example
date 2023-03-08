1224fd79a161007cfbec4969bd5107adedacca96
George Davis <george@triumphtech.com>
2023-03-07 23:14:08 -0800

    refactor: Fix capitalization

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    // Importing our AWS Libraries
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+	// Importing our AWS Libraries
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-    // lambda.Start is how Lambda handles passing various events into your code
-    lambda.Start(handler)
+	// lambda.Start is how Lambda handles passing various events into your code
+	lambda.Start(handler)
 }
 
 // 'handler' will change the inputs & outputs depending on the type of event you trying to handle
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    // Logging a simple message to the console, which will display in the AWS Console
-    log.Println("hello world!")
+	// Logging a simple message to the console, which will display in the AWS Console
+	log.Println("Hello world!")
 
-    // Creating our response with a status code of 200
-    response := events.APIGatewayProxyResponse{
-        StatusCode: 200,
-    }
-    
-    // Finally, return the response with a nil error object to satisfy the outputs
-    return response, nil
+	// Creating our response with a status code of 200
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}
+
+	// Finally, return the response with a nil error object to satisfy the outputs
+	return response, nil
 }
